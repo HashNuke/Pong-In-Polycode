99ae31b4f200f79fea76f487d2e87b4f7142228b
Akash Manohar J <akash@akash.im>
2011-07-19 17:59:26 +0530

    added ball class

--- project.lua
+++ project.lua
@@ -9,5 +9,19 @@ function Player:Player(x,y)
     screen:addChild(self.shape)
 end
 
+-- end of player class
+
+class "Ball" (EventHandler)
+
+function Ball:Ball
+    self.shape = ScreenShape(SHAPE_CIRCLE, 10, 10)
+    self.shape:setPositionMode(POSITION_CENTER)
+    self.shape:setPosition(320,240)
+    screen:addChild(self.shape)
+end
+
+-- end of Ball class
+
 human = Player(10,50)
 computer = Player(630,50)
+ball = Ball()
