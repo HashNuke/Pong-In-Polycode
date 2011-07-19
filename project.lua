f5d44589db9ec14bded1766f075d844e51265c2f
Akash Manohar J <akash@akash.im>
2011-07-19 23:12:55 +0530

    minor change
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- project.lua
+++ project.lua
@@ -56,8 +56,9 @@ class "Ball" (EventHandler)
 
 function Ball:Ball()
     self.shape = ScreenShape(SHAPE_CIRCLE, 10, 10)
-    --self.shape:setPositionMode(POSITION_CENTER)
+    self.shape:setPositionMode(POSITION_CENTER)
     self.shape:setPosition(320,240)
+    
     local friction = 0.1
     local density = 0       -- weight of the object
     local restitution = 5   -- controls bounce
