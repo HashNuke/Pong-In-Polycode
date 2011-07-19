e1455127fb1ab2762c256a25ea74d6e6244f74df
Akash Manohar J <akash@akash.im>
2011-07-19 18:30:02 +0530

    player input working
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- project.lua
+++ project.lua
@@ -2,11 +2,42 @@ screen = Screen()
 
 class "Player" (EventHandler)
 
-function Player:Player(x,y)
+function Player:Player(x, y, human_player)
     self.shape = ScreenShape(SHAPE_RECT, 10, 100)
     self.shape:setPositionMode(POSITION_CENTER)
     self.shape:setPosition(x,y)
     screen:addChild(self.shape)
+
+    if human_player == true then
+        self:EventHandler()
+
+        Services.Core:getInput():addEventListener(self, EVENT_KEYUP)
+        Services.Core:getInput():addEventListener(self, EVENT_KEYDOWN)
+    end
+        
+end
+
+function Player:handleEvent(e)
+    if e:getDispatcher() == Services.Core:getInput() then
+        local inputEvent = InputEvent(e)
+        local key = inputEvent:keyCode()
+        
+        if e:getEventCode() == EVENT_KEYDOWN then
+            if key == KEY_UP then
+                local pos = self.shape:getPosition()
+                
+                -- NOTES: screen measurement is from top(0) to down(n)
+                self.shape:setPosition(pos.x, pos.y-25)
+            elseif key == KEY_DOWN then
+                local pos = self.shape:getPosition()
+                self.shape:setPosition(pos.x, pos.y+25)
+            end
+        elseif e:getEventCode() == EVENT_KEYUP then
+            if key == KEY_DOWN or key == KEY_UP then
+                -- do something
+            end
+        end
+    end
 end
 
 -- end of player class
@@ -22,6 +53,6 @@ end
 
 -- end of Ball class
 
-human = Player(10,50)
-computer = Player(630,50)
+human = Player(10, 50, true)
+computer = Player(630, 50, false)
 ball = Ball()
