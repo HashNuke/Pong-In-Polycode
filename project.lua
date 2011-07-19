88cff3cfe1cb1d8f8c2b838ebf8b887a148c60af
Akash Manohar J <akash@akash.im>
2011-07-19 17:51:54 +0530

    added player class; now displays one player
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ project.lua
@@ -0,0 +1,13 @@
+screen = Screen()
+
+class "Player" (EventHandler)
+
+function Player:Player(x,y)
+    self.shape = ScreenShape(SHAPE_RECT, 10, 100)
+    self.shape:setPositionMode(POSITION_CENTER)
+    self.shape:setPosition(x,y)
+    screen:addChild(self.shape)
+end
+
+human = Player(10,50)
+
